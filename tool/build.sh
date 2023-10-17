2952d02797fd4b1bb1adf8ab744a9d21dda3207a
Tom Coder <vantoan8x@gmail.com>
2023-10-17 10:11:22 +0700

    update script

--- tool/build.sh
+++ tool/build.sh
@@ -21,39 +21,40 @@ cp ${dist}/index.html ./ > /dev/null
 cp ${dist}/index.bundle.js ./ > /dev/null
 cp ${dist}/index.bundle.js.LICENSE.txt ./ > /dev/null
 
-cd ${destFolder}/../..
 
 # Copy to Web-Front
 export webfrontPath="${curDir}/../PMS-WebFront/public"
 if [[ -d "$webfrontPath" ]]; then
     mkdir -p ${webfrontPath}/ > /dev/null
-    cp -R ./release/mermaid ${webfrontPath}/ > /dev/null
+    cp -R ${destFolder} ${webfrontPath}/ > /dev/null
 fi
 
 # Copy to Wrapper.Dev
 export desktopPath="${curDir}/../PMS-Wrapper/build.dev/src/main/web/public"
 if [[ -d "$desktopPath" ]]; then
     mkdir -p ${desktopPath}/ > /dev/null
-    cp -R ./release/mermaid ${desktopPath}/ > /dev/null
+    cp -R ${destFolder} ${desktopPath}/ > /dev/null
 fi
 
 # Copy to Wrapper.Prod
 export desktopPath="${curDir}/../PMS-Wrapper/build.prod/src/main/web/public"
 if [[ -d "$desktopPath" ]]; then
     mkdir -p ${desktopPath}/ > /dev/null
-    cp -R ./release/mermaid ${desktopPath}/ > /dev/null
+    cp -R ${destFolder} ${desktopPath}/ > /dev/null
 fi
 
 # Copy to Wrapper Local Dev
 export desktopPath="${curDir}/../PMS-Wrapper/src/main/web/public"
 if [[ -d "$desktopPath" ]]; then
     mkdir -p ${desktopPath}/ > /dev/null
-    cp -R ./release/mermaid ${desktopPath}/ > /dev/null
+    cp -R ${destFolder} ${desktopPath}/ > /dev/null
 fi
 
 # Copy to Mobile App
 export mobilePath="${curDir}/../PMS-Mobile-Core/assets/web/public"
 if [[ -d "${mobilePath}" ]]; then
     mkdir -p ${mobilePath}/ > /dev/null
-    cp -R ./release/mermaid ${mobilePath}/ > /dev/null
+    cp -R ${destFolder} ${mobilePath}/ > /dev/null
 fi
+
+cd ${curDir}
