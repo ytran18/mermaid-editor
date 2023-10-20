a82042e41a998e881e19b7d7f694f299b73ad737
ytran <a11718trannhuy@gmail.com>
2023-10-20 12:43:15 +0700

    update code

--- src/components/mermaid.js
+++ src/components/mermaid.js
@@ -2,8 +2,6 @@ import React, { useEffect, useRef, useState } from "react";
 import { TransformComponent, TransformWrapper } from 'react-zoom-pan-pinch';
 import mermaid from 'mermaid'
 
-import { ReactComponent as IconClose } from '../assets/icons/iconClose.svg';
-
 import './style.css';
 
 mermaid.initialize({
@@ -120,7 +118,9 @@ const Mermaid = React.forwardRef((props, ref) => {
                     </div>
                 </div>
                 <div className="close-btn">
-                    <IconClose onClick={handleCLoseBtn}/>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" width='20' heigh='20' viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6" onClick={handleCLoseBtn}>
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+                    </svg>
                 </div>
             </div>
             <TransformWrapper>
