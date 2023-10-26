62816655827af84b3c9a7c4a48c099205ef1a866
ytran <a11718trannhuy@gmail.com>
2023-10-26 14:59:31 +0700

    add load document function when in action tab

--- src/components/editor.js
+++ src/components/editor.js
@@ -187,7 +187,7 @@ const Editor = (props) => {
                         <path stroke-linecap="round" stroke-linejoin="round" d="M6.75 12a.75.75 0 11-1.5 0 .75.75 0 011.5 0zM12.75 12a.75.75 0 11-1.5 0 .75.75 0 011.5 0zM18.75 12a.75.75 0 11-1.5 0 .75.75 0 011.5 0z" />
                     </svg>
                     {state.showActions && (
-                        <Actions handleDownload={handleDownload}/>
+                        <Actions handleDownload={handleDownload} handleDocument={handleDocument}/>
                     )}
                 </div>
                 <div className={`top-right ${state.divWidth < 510 ? 'hidden': ''}`}>
