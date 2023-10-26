4002d2fb6bde5216a19ab99ace95d86ba7c903e8
ytran <a11718trannhuy@gmail.com>
2023-10-26 13:32:41 +0700

    fix monaco error when resize mermaid

--- src/components/editor.js
+++ src/components/editor.js
@@ -67,6 +67,23 @@ const Editor = (props) => {
         monaco.editor.setTheme('mermaid')
         initEditor(monaco);
 
+        const resizeObserver = new ResizeObserver((entries) => {
+            requestAnimationFrame(() => {
+                editorRef.current?.layout({
+                  height: entries[0].contentRect.height,
+                  width: entries[0].contentRect.width
+                });
+              });
+        });
+
+        if (editorContainer) {
+            resizeObserver.observe(editorContainer.parentElement);
+        };
+
+        window.addEventListener("error", function (e) {
+            console.error("MU: ",e.message);
+        });
+
         return () => {
             editorRef.current.dispose();
         }
@@ -80,7 +97,6 @@ const Editor = (props) => {
             // get value of first line
             const firstLineText = model.getValue().split('\n')[0];
             const letters = firstLineText?.split(' ')?.[0]?.replace(/[^a-zA-Z0-9]/g, '')
-            console.log(letters.trim());
             const position = model.getPositionAt(model.getValueLength());
             editorRef.current.setPosition(position);
             setState(prev => ({...prev, count: prev.count + 1, url: letters.trim()}));
@@ -111,10 +127,9 @@ const Editor = (props) => {
             const model = editorRef.current.getModel();
             const firstLineText = model.getValue().split('\n')[0];
             const letters = firstLineText?.split(' ')?.[0]?.replace(/[^a-zA-Z0-9]/g, '')
-            console.log("Code changes: ", letters.trim());
             setState(prev => ({...prev, count: prev.count + 1, url: letters.trim()}));
         }
-    },[code])
+    },[code]);
 
     const handleDocument = () => {
         const openurl = {
@@ -185,7 +200,7 @@ const Editor = (props) => {
                         <svg xmlns="http://www.w3.org/2000/svg" width='18' height='18' fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M9 12h3.75M9 15h3.75M9 18h3.75m3 .75H18a2.25 2.25 0 002.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 00-1.123-.08m-5.801 0c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 00.75-.75 2.25 2.25 0 00-.1-.664m-5.8 0A2.251 2.251 0 0113.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m0 0H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V9.375c0-.621-.504-1.125-1.125-1.125H8.25zM6.75 12h.008v.008H6.75V12zm0 3h.008v.008H6.75V15zm0 3h.008v.008H6.75V18z" />
                         </svg>
-                        DOCS
+                        DOC
                     </div>
                 </div>
             </div>
